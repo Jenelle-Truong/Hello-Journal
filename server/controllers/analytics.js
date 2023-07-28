51efb1bef1be849adfaa0398c097908dd168c4ba
Jenelle Truong <jftruong@ucsd.edu>
2023-07-28 01:24:04 -0700

    cleaned up unused code to reduce memory consumption

--- server/controllers/analytics.js
+++ server/controllers/analytics.js
@@ -21,28 +21,19 @@ export const analyzeSentiment = async (req, res) => {
         const { userId } = req.params;
         const user = await User.findById(userId);
         const mostRecentPosts = user.toObject().entries
-        
-        console.log('most recent posts')
 
         const classifier = await SentimentAnalysisPipeline.getInstance()
         var data = []
         for (let i = 0; i < mostRecentPosts.length; i++) {
             let post = await Entry.findById(mostRecentPosts[i]);
-            // dates.push(post.createdAt);
             let text = post.label;
-            console.log(text);
             // cached classifier returns JSON array of size 1 containing the most probable sentiment
             let response = await classifier(text);
-            console.log(response);
             let { label } = response[0];
             let sentiment = Number(label.slice(0, 1)); 
-            // responses.push(sentiment); 
             let date = new Date(post.year, post.month, post.day)
-            console.log(`${date}`)
-            console.log(sentiment)
             data.push({ date: date,  rank: sentiment , label: post.label });
         }
-        console.log(`data ${data}`)
         res.status(200).json(data)
     } catch (err) {
         res.status(404).json({ message: err.message })
