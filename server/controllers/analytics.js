6606148baa42578e05eaa0ae3c3b7d4f70200269
Jenelle Truong <jftruong@ucsd.edu>
2023-07-28 11:35:15 -0700

    refactored code to leverage HF inference API

--- server/controllers/analytics.js
+++ server/controllers/analytics.js
@@ -1,20 +1,5 @@
-import { pipeline } from "@xenova/transformers"
 import User from "../models/User.js";
 import Entry from "../models/Entry.js";
-
-class SentimentAnalysisPipeline {
-    static task = "text-classification"
-    static model = "Xenova/bert-base-multilingual-uncased-sentiment"
-    static instance = null;
-
-    static async getInstance(progress_callback = null) {
-        if (this.instance == null) {
-            this.instance = pipeline(this.task, this.model, { progress_callback });
-        }
-
-        return this.instance;
-    }
-}
 
 export const analyzeSentiment = async (req, res) => {
     try {
@@ -22,20 +7,42 @@ export const analyzeSentiment = async (req, res) => {
         const user = await User.findById(userId);
         const mostRecentPosts = user.toObject().entries
 
-        const classifier = await SentimentAnalysisPipeline.getInstance()
         var data = []
         for (let i = 0; i < mostRecentPosts.length; i++) {
             let post = await Entry.findById(mostRecentPosts[i]);
             let text = post.label;
-            // cached classifier returns JSON array of size 1 containing the most probable sentiment
-            let response = await classifier(text);
-            let { label } = response[0];
-            let sentiment = Number(label.slice(0, 1)); 
+            let response = await fetch(
+                "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment",
+                {
+                    method: "POST", 
+                    headers: { Authorization: `Bearer ${process.env.HF_TOKEN}`},
+                    body: JSON.stringify(text)
+                }
+            )
+            let responseData = await response.json();
+            console.log("response data");
+            console.log(responseData);
+            let maxScore = 0;
+            let sentiment = "";
+            console.log(responseData[0].length)
+            for (let j = 0; j < responseData[0].length; j++) {
+                console.log(responseData[0][j])
+                let { label, score } = responseData[0][j];
+                console.log(`sentiment label ${label}`);
+                console.log(score)
+                if (score > maxScore) {
+                    maxScore = score
+                    sentiment = Number(label.slice(0, 1));
+                    console.log(`in if statement ${sentiment}`); 
+                }
+            }
+            console.log(sentiment)
             let date = new Date(post.year, post.month, post.day)
             data.push({ date: date,  rank: sentiment , label: post.label });
         }
         res.status(200).json(data)
     } catch (err) {
+        console.log(err.message)
         res.status(404).json({ message: err.message })
     }
 }
